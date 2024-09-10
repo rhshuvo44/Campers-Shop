7e7a951b09dca2936bbaa215081af5bd6a466255
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-09-10 13:54:37 +0600

    navbar and footer

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
